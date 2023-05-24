9a1fbda91f925afc563e4cd3f3912bfec373f840
JulieVedel <Juli76k0@edu.easj.dk>
2023-05-24 13:18:21 +0200

    Face detect

---
+++ Face_Detector.py
@@ -0,0 +1,19 @@
+import cv2
+
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+img = cv2.imread('Angelina.jpg')
+
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+
+cv2.imshow('Program', img)
+cv2.waitKey()
+
+print("Code Completed")
